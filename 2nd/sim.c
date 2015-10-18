e284921cf497964f36e1d17f42fd9ec874c0656e
Archimedes of Syracuse <archimedes@syracuse.(none)>
2015-10-18 15:59:58 +0100

    rettede fejl, indsatte bne og begyndte på jump

--- 2nd/sim.c
+++ 2nd/sim.c
@@ -11,22 +11,22 @@
 #define ERROR_FSCANF "Fscanf failed\n"
 #define ERROR_READ_CONFIG "Config could not be read\n"
 #define ERROR_J "Error occured when trying to jump\n"
+#define ERROR_ELF_DUMP "Error occured in elf_dump()\n"
+#define ERROR_INTERP "Error occured in interp()\n"
 #define ERROR_UNKNOWN_INSTRUCTION -3
 #define ERROR_UNKNOWN_OPCODE -2
 #define ERROR_UNKNOWN_FUNCT -4
-#define ERROR_INTERP_CONTROL -1
-#define ERROR_INTERP_ID -2
-#define ERROR_INTERP_EX -3
-#define ERROR_INTERP_MEM -4
+#define ERROR_INTERP_CONTROL -5
+#define ERROR_INTERP_ID -6
+#define ERROR_INTERP_EX -7
+#define ERROR_INTERP_MEM -8
 #define SAW_SYSCALL -1
-
 
 #define AT regs[1]
 #define V0 regs[2]
 #define V1 regs[3]
 #define SP regs[29]
 #define RA regs[31]
-/*INSTRUKTOR 0: MEMSZ is perfectly right, but you could have used the hex value 0xA0000 instead */
 #define MEMSZ 0xA0000
 
 struct preg_if_id {
@@ -42,6 +42,8 @@ struct preg_id_ex {
   bool alu_src;
   bool mem_to_reg;
   bool branch;
+  bool beq;
+  bool jump;
 
   uint32_t reg_dst;
   uint32_t rt;
@@ -60,6 +62,8 @@ struct preg_ex_mem {
   bool reg_write;
   bool mem_to_reg;
   bool branch;
+  bool beq;
+  bool jump;
  
   uint32_t reg_dst;
   uint32_t rt;
@@ -116,10 +120,6 @@ int read_config_stream(FILE *stream) {
 
     for (i = 8; i < 16; i++){
       uint32_t v;
-    /*  INSTRUKTOR  -1:the return value of fscanf is the number of input items
-        successfully matched and assigned. Use this to make sure that fscanf return the right
-        ourput, and return the function with an error-code, if you do not get
-        the right number of outputs. */
       int fScanF = fscanf(stream, "%u", &v);
       if (fScanF == 1) {
         regs[i] = v;
@@ -139,9 +139,6 @@ int read_config(const char *path) {
   FILE *stream = fopen(path, "r");
   int read = -1;
   if (stream != NULL){
-
-  /* INSTRUKTOR -2:  this function have a return value. Use this to test wether or not
-      any error occured during the reading of the config */
     read = read_config_stream(stream);
   }
   else{
@@ -157,259 +154,6 @@ int read_config(const char *path) {
     return -3;
   }
   return read;
-
-      /* INSTRUKTOR -2: This part of the code is never run
-     * this is because you return in both the if-clause and the else-clause.
-     * you should not return 0, before you know that everything went well.
-     */
-
-  /* INSTRUKTOR 1: Use this style of coding for all you sanity checks  */
-  /* INSTRUCTOR If something went wrong, you want to let the caller know,
-   * And therby making it possible to halt the program without other instructions get run. */
-}
-
-
-/* Executes the R instructions */
-int interp_r (uint32_t inst){
-
-  uint32_t funct = GET_FUNCT(inst);
-  uint32_t rs, rt, rd, shamt;
-  rs    = GET_RS(inst);
-  rt    = GET_RT(inst);
-  rd    = GET_RD(inst);
-  shamt = GET_SHAMT(inst);
-
-  switch(funct)
-  {
-    case FUNCT_JR :
-      /* INSTRUKTOR -2: Because you are incrementing PC
-       * in interp(), then you will be off-by-one/four */
-
-       /* We get the instruction before incrementing PC and should
-        * therefore not be off, if this is incorrect, please explain in detail.
-        */
-      PC = regs[rs];
-      break;
-
-    case FUNCT_SYSCALL :
-    /* INSTRUKTOR 0: no code after a return-statement will be run,
-     * if you return here, you dont need a break-statement.
-     * It would have been nicer for you to define the syscall-code, so you dont have any
-     * 'magic numbers' */
-      return SAW_SYSCALL;
-
-    case FUNCT_ADDU :
-      regs[rd] = regs[rs] + regs[rt];
-      break;
-
-    case FUNCT_ADD :
-      regs[rd] = regs[rs] + regs[rt];
-      break;
-
-    case FUNCT_SUB :
-      regs[rd] = regs[rs] - regs[rt];
-      break;
-
-    case FUNCT_SUBU :
-      regs[rd] = regs[rs] - regs[rt];
-      break;
-
-    case FUNCT_AND :
-      regs[rd] = regs[rs] & regs[rt];
-      break;
-
-    case FUNCT_OR :
-      regs[rd] = regs[rs] | regs[rt];
-      break;
-
-    case FUNCT_NOR :
-      regs[rd] = ~(regs[rs] | regs[rt]);
-      break;
-
-    case FUNCT_SLT :
-      if (regs[rs] < regs[rt])
-        regs[rd] = 1;
-      else 
-        regs[rd] = 0;
-      break;
-
-    case FUNCT_SLL :
-      regs[rd] = regs[rt] << shamt;
-      break;
-
-    case FUNCT_SRL :
-      regs[rd] = regs[rt] >> shamt;
-      break;
-
-    default:
-      return ERROR_UNKNOWN_INSTRUCTION;
-  /* INSTRUKTOR -1: You need a default case, look at interp for inspiration */
-  }
-  
-  return 0;
-}
-
-// Jumps the PC to an address contained in the instruction
-int j(uint32_t inst){
-
-  // The top 4 bits of the PC 
-
-  /* INSTRUKTOR -2: By shifting left, you are removing the 26 most significant bits
-   * You want to keep the 4 most significant bits instead.
-   * recall that MS_4B gives you a mask looking like 1111000[...]0. 
-   * what happens when you use the or-operator on something that have 1's?
-   * think of how you can use the and-operator to fix this instead.
-   * Think of the difference between
-   * {(PC+4[31:28], address, 2'b0)} (the right one) and
-   * {(PC[31:28], address, 2'b0) + 4} (almost your version)
-   * this might cause you to change your interp()
-   */
-  uint32_t upperPC  = PC & MS_4B;
-  
-  // The address provided in the MIPS j instruction
-  uint32_t address  = GET_ADDRESS(inst);
-
-  // The top 4 bits of the pc concatenated with the address,
-  // concatenated with 00, thus creating the Pseudo-direct address
-  uint32_t PD_address = upperPC | (address << 2);
-  printf("PD_ADDRESS: %x\n",PD_address); 
-  PC = PD_address;
-
-  return 0;
-}
-
-// Jumps the pc to an address contained in the instruction,
-// and sets the $ra to the next instruction
-int jal(uint32_t inst){
-  // Sets the return address to the following instruction.
-  // PC is incremented immediately after reading the instruction in interp()
-  // and thus we do not need to increment it now to get the next instruction
-  RA = PC;
-
-  j(inst);
-
-  return 0;
-}
-
-// Identifies the instruction by looking at the opcode,
-// then performs the instruction
-int interp_inst (uint32_t inst){
-
-  uint32_t opcode = GET_OPCODE(inst);
-  uint32_t rs, rt, imm;
-  int retval = 0;
-  switch(opcode)
-  {
-    case OPCODE_R :
-      retval = interp_r(inst);
-      break;
-    
-    case OPCODE_J :
-      ; // empty statement hack
-      /* INSTRUKTOR -1: use the return value */
-      int jump = j(inst);
-      if (jump != 0) {
-        printf(ERROR_J);
-      }
-      break;
-    
-    case OPCODE_JAL :
-      jal(inst);
-      break;
-     
-    case OPCODE_BEQ :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      if (regs[rs] == regs[rt]) {
-        /* INSTRUKTOR -2: remember to signextend */
-        PC = PC + 4* imm;
-      }
-      break;
-
-    case OPCODE_BNE :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      if (regs[rs] != regs[rt]) {
-        PC = PC + 4*imm;
-      }
-      break;
-
-    case OPCODE_ADDI :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and sign-extends
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      regs[rt] = regs[rs] + imm;
-      break;
-
-    case OPCODE_ADDIU :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and sign-extends
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      regs[rt] = regs[rs] + imm; 
-      break;
-
-    case OPCODE_SLTI :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and sign-extends
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      if (regs[rs] < imm) {
-        regs[rt] = 1;
-      }
-      else {
-        regs[rt] = 0;
-      }
-      break;
-
-    case OPCODE_ANDI :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and zero-extends
-      imm = ZERO_EXTEND(GET_IMM(inst));
-      regs[rt] = regs[rs] & imm;
-      break;
-
-    case OPCODE_ORI :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and zero-extends
-      imm = ZERO_EXTEND(GET_IMM(inst));
-      regs[rt] = regs[rs] | imm;
-      break;
-      
-    case OPCODE_LUI :
-      rt  = GET_RT(inst);
-      // gets imm and left-shifts by 16 bits
-      imm = GET_IMM(inst) << 16;
-      regs[rt] = imm;
-      break;
-
-    case OPCODE_LW :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and sign-extends
-      imm     = SIGN_EXTEND(GET_IMM(inst));
-      uint32_t loadAddress = regs[rs] + imm;
-      regs[rt] = GET_BIGWORD(mem, loadAddress);
-      break;
-
-    case OPCODE_SW :
-      rs  = GET_RS(inst);
-      rt  = GET_RT(inst);
-      // gets imm and sign-extends
-      imm = SIGN_EXTEND(GET_IMM(inst));
-      uint32_t storeAddress  = regs[rs] + imm; 
-      SET_BIGWORD(mem, storeAddress, regs[rt]);
-      break;
-
-    default :
-      retval = ERROR_UNKNOWN_OPCODE; 
-  }
-  return retval;
 }
 
 int interp_control(){
@@ -424,19 +168,26 @@ int interp_control(){
       id_ex.rt_value      = regs[id_ex.rt];
       id_ex.funct         = GET_FUNCT(if_id.inst);
       id_ex.shamt         = GET_SHAMT(if_id.inst);
+      
+    case  FUNCT_JR :
+      // something
+      
       break;
 
     case OPCODE_BEQ :
       id_ex.branch        = true;
+      id_ex.beq           = true;
       id_ex.rt            = GET_RT(if_id.inst);
       id_ex.rs_value      = regs[GET_RS(if_id.inst)];
       id_ex.rt_value      = regs[id_ex.rt];
       id_ex.sign_ext_imm  = SIGN_EXTEND(GET_IMM(if_id.inst));
+      // INSTRUKTOR 0: no reason to updates fields that you dont use
       id_ex.funct         = FUNCT_SUB;
       break;
     
     case OPCODE_BNE :
       id_ex.branch        = true;
+      id_ex.beq           = false;
       id_ex.rt            = GET_RT(if_id.inst);
       id_ex.rs_value      = regs[GET_RS(if_id.inst)];
       id_ex.rt_value      = regs[id_ex.rt];
@@ -466,7 +217,7 @@ int interp_control(){
       id_ex.sign_ext_imm  = SIGN_EXTEND(GET_IMM(if_id.inst));
       id_ex.funct         = FUNCT_ADD;
       break;
-
+    // INSTRUKTOR -2: Make cases for all I-type and J-type instructions
     default:
       printf("ERROR: Unknown opcode in interp_control()\n"); 
       return ERROR_UNKNOWN_OPCODE; 
@@ -491,7 +242,10 @@ int interp_id() {
   id_ex.sign_ext_imm  = 0;
   id_ex.funct         = 0;
   id_ex.shamt         = 0;
-  id_ex.next_pc       = if_id.next_pc;
+  // INSTRUKTOR 0: you might as well instantiate all the fields
+  // (not control bits) right away. If some instructions 'hack'
+  // any of the fields, then you just overwrite them in the controller
+  id_ex.next_pc       = if_id.next_pc; 
   int retControl = interp_control();
   if (retControl != 0){
     printf("ERROR: interp_control() failed\n");
@@ -502,6 +256,10 @@ int interp_id() {
 
 
 int alu() {
+  // INSTRUKTOR 0: instead of the if-else case, then simply
+  // add a variable and use this as second operand in all operations.
+  // Then you can make a simple test here, to see if
+  // it should hold the imm. value or the rt_value.
   if (id_ex.alu_src) {
     switch (id_ex.funct) {
       case (FUNCT_ADD):
@@ -516,9 +274,6 @@ int alu() {
   else {
     switch (id_ex.funct) {
       case (FUNCT_ADD):
-        ex_mem.alu_res = id_ex.rs_value + id_ex.rt_value;
-        break;     
- 
       case (FUNCT_ADDU):
         ex_mem.alu_res = id_ex.rs_value + id_ex.rt_value;
         break;
@@ -538,7 +293,19 @@ int alu() {
       case (FUNCT_SLL):
         ex_mem.alu_res = id_ex.rt_value << id_ex.shamt;
         break;
-     
+    
+      case (FUNCT_SLT):
+        ; // Empty statement
+        int signed_rs_value = id_ex.rs_value;
+        int signed_rt_value = id_ex.rt_value;
+        if (signed_rs_value < signed_rt_value) {
+          ex_mem.alu_res = 1;
+        }
+        else {
+          ex_mem.alu_res = 0;
+        }
+        break;
+ 
       case (FUNCT_SLTU):
         if (id_ex.rs_value < id_ex.rt_value) {
           ex_mem.alu_res = 1;
@@ -547,7 +314,11 @@ int alu() {
           ex_mem.alu_res = 0;
         }
         break;
- 
+
+      // INSTRUKTOR -2: How about slt? 
+      // When implementing this case, be aware of the difference
+      // of signed and unsigned comparisons.
+      // (1101 will fx be 13 in unsigned version and -3 in signed version) 
       case (FUNCT_SRL):
         ex_mem.alu_res = id_ex.rt_value >> id_ex.shamt;
         break;
@@ -560,9 +331,13 @@ int alu() {
         ex_mem.alu_res = id_ex.rs_value - id_ex.rt_value;
         break;
   
+      case (FUNCT_JR):
+        // Something
+        break;
+      
       case (FUNCT_SYSCALL):
         return SAW_SYSCALL;
-
+      
       default:
         printf("ERROR: Unknown funct in alu()\n");
         return ERROR_UNKNOWN_FUNCT;
@@ -576,6 +351,8 @@ int interp_ex(){
   ex_mem.mem_write      = id_ex.mem_write;
   ex_mem.reg_write      = id_ex.reg_write;
   ex_mem.branch         = id_ex.branch;
+  ex_mem.beq            = id_ex.beq;
+  ex_mem.jump           = id_ex.jump;
   ex_mem.rt             = id_ex.rt; 
   ex_mem.rt_value       = id_ex.rt_value;
   ex_mem.mem_to_reg     = id_ex.mem_to_reg;
@@ -584,10 +361,8 @@ int interp_ex(){
   
 
   int retAlu = alu();
-  if (retAlu == SAW_SYSCALL) {
-    // success
-  }
-  else if (retAlu != 0) {
+  
+  if (retAlu != SAW_SYSCALL && retAlu != 0) {
     printf("ERROR: alu() failed\n");
   }
   
@@ -612,14 +387,14 @@ void interp_mem(){
 void interp_wb(){
   if (mem_wb.reg_dst == 0 || !mem_wb.reg_write){
     // Do nothing
+    return;
   }
+  
+  if (mem_wb.mem_to_reg) {
+    regs[mem_wb.reg_dst] = mem_wb.read_data;
+  } 
   else {
-    if (mem_wb.mem_to_reg) {
-      regs[mem_wb.reg_dst] = mem_wb.read_data;
-    } 
-    else {
-      regs[mem_wb.reg_dst] = mem_wb.alu_res;
-    }
+    regs[mem_wb.reg_dst] = mem_wb.alu_res;
   }
 }
 
@@ -637,6 +412,7 @@ int cycle(){
   interp_wb();
   interp_mem();
   int retEx = interp_ex();
+  
   if (retEx == SAW_SYSCALL){
     retval = SAW_SYSCALL;
   }
@@ -650,29 +426,37 @@ int cycle(){
     return ERROR_INTERP_ID;
   }
   interp_if();
-  if (ex_mem.branch && ex_mem.alu_res == 0) {
+  if (ex_mem.branch) {
+    if ((ex_mem.beq && ex_mem.alu_res == 0) || (!ex_mem.beq && ex_mem.alu_res != 0)){ 
+      PC = ex_mem.branch_target;
+      if_id.inst = 0;
+      instr_cnt--;
+    } 
+  }
+  if (ex_mem.jump) {
     PC = ex_mem.branch_target;
     if_id.inst = 0;
-    instr_cnt--;
+    instr_cnt--; 
   }
+    
   return retval;
 }
 
 // Runs an infinite loop and increments the PC
 // after dealing with an instruction.
-// The loop only ends on a syscall
+// The loop only ends on a syscall or an error
 int interp(){
-
+  int retCycle = 0;
   int stop = 0;
 
   while(!stop){
   
     cycles++;
-    int retCycle = cycle();
-    if (retCycle == SAW_SYSCALL || retCycle == ERROR_INTERP_ID)
+    retCycle = cycle();
+    if (retCycle != 0)
       stop = 1;
   }
-  return 0;
+  return retCycle;
 }
 
 // Gets the cfg from the command line, reads it, updates the registers,
@@ -684,30 +468,32 @@ int main(int argc, char *argv[]) {
 int read;
   if (argc == 3) {
     read = read_config(argv[1]);
-    // ^^^^^^^^^^^^^^^^^
-    // INSTRUKTOR -: Remember to _always_ check the return value of any function to
-    // check that no error occurred.  (that is why you have a return value in c)
   }
   else {
-    /* INSTRUKTOR 0:  Your message should help the user, letting them know
-     * What kind of first- and second argument should be given to this program */
     printf(ERROR_INVALID_ARGS);
     return -1;
   }
-  /* INSTRUKTOR -1: this part of the code should only be run, it the right
-   * arguments are given. */
-  if (read == 0) {
-    elf_dump(argv[2], &PC, &mem[0], MEMSZ);
 
-  /* INSTRUKTOR 0: change the initial value of sp as explained in the 
-   * feedback */
+  if (read == 0) {
+    int retElfDump = elf_dump(argv[2], &PC, &mem[0], MEMSZ);
+    
+    if(retElfDump != 0) {
+      printf(ERROR_ELF_DUMP);
+      return -2;
+    }
+    
     SP = MIPS_RESERVE + MEMSZ;
-    interp();  
+    
+    int retInterp = interp();
+    if (retInterp != SAW_SYSCALL && retInterp != 0) {
+      printf(ERROR_INTERP);
+      return -3;
+    }  
     show_status();
   }
   else {
     printf(ERROR_READ_CONFIG);
-    return -2;
+    return -4;
   }
   return 0;
 }
